145d79f428ac3f28cdfa80b86aa9ce4870bc0d8f
andovnar2021 <87485716+andovnar2021@users.noreply.github.com>
2022-06-11 18:35:12 +0300

    Update mytest.py

--- mytest.py
+++ mytest.py
@@ -1,22 +1,6 @@
 
-from myapp import searchletters, client
 
-def test_searchletters():
-   assert {'e','o'} == searchletters("hello","aeoiu")
-    
-    
 
-def test_main_page():
-   res = client.get("/")
-   assert res.status_code == 200
-
-def test_login():
-   res = client.get("/login")
-   assert res.status_code == 200
-
-def test_logout():
-   res = client.get("/logout")
-   assert res.status_code == 200
     
 def test_simple3():
     mylist = [1,3,5]
